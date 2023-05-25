51b0df88dfa4687bd4b6598d7ed9994223854e3d
samuel merchan <samuelmerchan4@gmail.com>
2023-05-25 16:27:23 -0500

    segundo commit

--- src/people/admin.py
+++ src/people/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from people.models import Persona
 
 # Register your models here.
+class AdminPersona(admin.ModelAdmin):
+    list_display = ["_str_","nombres", "apellidos","email"]
+    list_filter = ["nombres", "apellidos", "email"]
+    list_editable = ["nombres", "apellidos", "email"]
+    class Meta:
+        model = Persona
+admin.site.register(Persona, AdminPersona)
--- src/people/models.py
+++ src/people/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Persona(models.Model):
+    nombres = models.CharField(max_length=100, null=False, blank=False)
+    apellidos = models.CharField(max_length=100, null=False, blank=False)
+    email = models.EmailField()
+    fecha_creacion = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    class Meta: db_table = "personas"
+
+    def _str_(self):
+        return f'{self.nombres} {self.apellidos} {self.email}'
